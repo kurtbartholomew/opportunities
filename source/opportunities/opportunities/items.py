298e10e0d4e4ecab57860b7c8cfdb29a19f2d564
Kurt Bartholomew <kurt.bartholomew@gmail.com>
2018-05-24 14:49:32 -0500

    (feat) Added to and refined selectors for capture ad page components

--- source/opportunities/opportunities/items.py
+++ source/opportunities/opportunities/items.py
@@ -16,10 +16,14 @@ class OpportunitiesItem(scrapy.Item):
     # selector for seperate ad items
     ITEM_SELECTOR = '//p[@class="result-info"]'
     # selector for title of ad
-    TITLE_SELECTOR = 'a[@class="result-title"]/text()'
-    # selector for date ad was posted (in format of YYYY-MM-DDTHH:MM:SS-MMMM)
+    TITLE_SELECTOR = 'a[contains(@class,"result-title")]/text()'
+    # selector for date ad was posted (in format of YYYY-MM-DD HH:MM)
     DATE_SELECTOR = 'time[@class="result-date"]/@datetime'
-    PAGINATION_REGEX = 'search\/\w+\?s='
+    # selector for url for additional crawling
+    URL_SELECTOR = 'a[contains(@class,"result-title")]/@href'
+    # regex to restrict the links that the crawler will follow
+    PAGINATION_REGEX = '.*search\/\w+.*'
+    #  selector to restrict area crawler will look for links to follow
     PAGINATION_SELECTOR = '//span[@class="buttons"]/a[@class="button next"]'
 
 
@@ -29,14 +33,14 @@ class AdItem(scrapy.Item):
     title = scrapy.Field()
     city = scrapy.Field()
     date = scrapy.Field()
-    map_address = scrapy.Field()
+    map_address_url = scrapy.Field()
     ad_attributes = scrapy.Field()
     ad_url = scrapy.Field()
 
     # selector for ad category in header of page
     CATEGORY_SELECTOR = '//nav[@class="breadcrumbs-container"]/ul/li[@class="crumb category"]/p/a/text()'
     # selector for body of main ad text
-    AD_POST_SELECTOR = 'section[@id="postingbody]"'
+    AD_POST_SELECTOR = '//section[@id="postingbody"]'
 
     # parent selector for most of ad
     AD_BODY_PARENT_SELECTOR = '//section[@class="body"]'
@@ -44,16 +48,20 @@ class AdItem(scrapy.Item):
     # parent selector for ad header
     HEADER_SECTION_PARENT_SELECTOR = '//span[@class="postingtitletext"]'
     # selector for title of ad
-    TITLE_SELECTOR = HEADER_SECTION_SELECTOR + 'span[@id="titletextonly]/text()"]'
+    TITLE_SELECTOR = HEADER_SECTION_SELECTOR + '/span[@id="titletextonly"]/text()'
     # selector for city of ad
-    AD_CITY_SELECTOR = HEADER_SECTION_SELECTOR + 'small/text()"]'
+    AD_CITY_SELECTOR = HEADER_SECTION_SELECTOR + '/small/text()'
 
     # selector for date ad was posted (in format of YYYY-MM-DDTHH:MM:SS-MMMM)
     DATE_SELECTOR = '//p[@id="display-date"]/time/@datetime'
     # selector for address within map (if provided)
     MAP_ADDRESS_SELECTOR = '//p[@class="mapaddress"]/small/a/@href'
+
     # selector for attributes of job (compensation, employment type, etc.)
-    AD_ATTRS_SELECTOR = '//p[@class="attrgroup"]/span'
+    AD_ATTRS_KEYS_SELECTOR = '//p[@class="attrgroup"]/span/text()'
+    AD_ATTRS_VALS_SELECTOR = '//p[@class="attrgroup"]/span/b/text()'
+    # regex to capture attribute keys properly
+    AD_ATTRS_REGEX = '^\w*'
     
 
 
--- source/opportunities/opportunities/spiders/lowkey.py
+++ source/opportunities/opportunities/spiders/lowkey.py
@@ -12,9 +12,45 @@ class LowkeySpider(CrawlSpider):
 
     def parse(self, response):
         
+        ads = Selector(response).xpath(OpportunitiesItem.ITEM_SELECTOR)
+
+        for ad in ads:
+            item = OpportunitiesItem()
+            item.title = ad.xpath(OpportunitiesItem.TITLE_SELECTOR).extract()
+            item.date = ad.xpath(OpportunitiesItem.DATE_SELECTOR).extract()
+            url = ad.xpath(OpportunitiesItem.URL_SELECTOR).extract()
+            # TODO: Add filters here
+            # if title does not include filter items
+            yield scrapy.Request(url, self.parse_ad)
+    
 
 
     def parse_ad(self, response):
+        ad = Selector(response)
+
+        item = AdItem()
+        item.category = ad.xpath(AdItem.CATEGORY_SELECTOR).extract()
+        item.ad_post = ad.xpath(AdItem.AD_POST_SELECTOR).extract()
+        main_section = ad.xpath(AdItem.AD_BODY_PARENT_SELECTOR)
+        item.title = main_section.xpath(AdItem.TITLE_SELECTOR).extract()
+        item.city = main_section.xpath(AdItem.AD_CITY_SELECTOR).extract()
+
+        # TODO: Parse date string into datetime object
+        item.date = main_section.xpath(AdItem.DATE_SELECTOR).extract()
+        item.map_address_url = main_section.xpath(AdItem.MAP_ADDRESS_SELECTOR).extract()
+
+        # TODO: Factor out into clean method and handle as items for seperate table
+        # For now, just turning into a string delimited by commas
+        attributes_keys = main_section.xpath(AdItem.AD_ATTRS_KEYS_SELECTOR)
+                                 .re(AdItem.Ad_ATTRS_REGEX)
+        attributes_vals = main_section.xpath(AdItem.AD_ATTRS_VALS_SELECTOR).extract()
+        attributes = ""
+        for i, key in enumerate(attributes_keys):
+            attributes += f'{key}: {attributes_vals[i]},'
+        
+
+        item.ad_url = response.url
+        yield item
     
 
     rules = [
